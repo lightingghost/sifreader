f3a1df996bb113fc003714d6febe32e304b895c5
lightingghost <zhenp3ngzhou@gmail.com>
2018-05-12 22:27:18 -0700

    add long description

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open(os.path.join(CURRENT_DIR, 'readme.md'), encoding='utf-8') as f:
 setup(
   name = 'sifreader',
   packages = ['sifreader'],
-  version = '0.2',
+  version = '0.2.1',
   install_requires = ['numpy'],
   description = 'A library to read Andor SIF file',
   long_description = long_description,
   author = 'Zhenpeng Zhou and Daniel R. Stephan',
   author_email = 'zhenp3ngzhou@gmail.com',
   url = 'https://github.com/lightingghost/sifreader',
-  download_url = 'https://github.com/lightingghost/sifreader/archive/0.2.tar.gz',
+  download_url = 'https://github.com/lightingghost/sifreader/archive/0.2.1.tar.gz',
   keywords = ['SIF', 'Andor', 'Image', 'reader'],
   classifiers = ['Development Status :: 3 - Alpha'],
 )
