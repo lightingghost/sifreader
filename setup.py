28e08c48d952c445e147c16f98773e8b8e55c841
lightingghost <zhenp3ngzhou@gmail.com>
2018-05-13 16:06:22 -0700

    fix an error in pip, remove long description temporarily

--- setup.py
+++ setup.py
@@ -4,21 +4,18 @@ import os
 
 from setuptools import setup
 
-CURRENT_DIR = os.path.dirname(__file__)
-with open(os.path.join(CURRENT_DIR, 'readme.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
   name = 'sifreader',
   packages = ['sifreader'],
-  version = '0.2.1',
+  version = '0.2.3',
   install_requires = ['numpy'],
   description = 'A library to read Andor SIF file',
-  long_description = long_description,
+  #long_description = open('readme.md').read(),
+  #long_description_content_type="text/markdown",
   author = 'Zhenpeng Zhou and Daniel R. Stephan',
   author_email = 'zhenp3ngzhou@gmail.com',
   url = 'https://github.com/lightingghost/sifreader',
-  download_url = 'https://github.com/lightingghost/sifreader/archive/0.2.1.tar.gz',
+  download_url = 'https://github.com/lightingghost/sifreader/archive/0.2.3.tar.gz',
   keywords = ['SIF', 'Andor', 'Image', 'reader'],
   classifiers = ['Development Status :: 3 - Alpha'],
 )
